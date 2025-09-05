f9a3ac76e483a1b4b42c865beaee4bcaaf15bb56
sean <seankuo.whysmile@gmail.com>
2025-09-06 05:59:06 +0800

    update

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ app.use(bodyParser.json());
 // MongoDB 連線
 mongoose.connect(process.env.MONGO_URI)
   .then(() => console.log("MongoDB connected"))
-  .catch(err => console.error(err));
+  .catch(err => console.error('error: ',err));
 
 
 // ================== RESTful API ==================
